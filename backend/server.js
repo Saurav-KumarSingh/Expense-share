fcea265638e9d3a1642de85c3d43c5e0e33fbcc8
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-10-22 14:14:11 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,17 @@ const db=require('./config/mongoose_connect')
 require('dotenv').config();
 const cors=require('cors')
 
+const corsOptions = {
+    origin: 'https://expense-share-five.vercel.app/', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true, 
+};
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors())
+app.use(cors(corsOptions));
 
 console.log(process.env.PORT)
 app.use('/api',router);
