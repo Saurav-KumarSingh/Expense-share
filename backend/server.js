cab989a12854bf719e01bc36a67fd643123fda1b
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-10-24 23:08:50 +0530

    _

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const cors=require('cors')
 
 const corsOptions = {
-    origin: 'https://expense-share-five.vercel.app/', 
+    origin: 'https://expense-share-five.vercel.app/*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true, 
 };
